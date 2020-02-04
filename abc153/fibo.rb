fd365d40fa80eb1d386e57ed22297358881789f8
takasaki <takasaki@inagora.com>
2020-02-04 21:03:06 +0900

    メモ化再帰サンプル

--- abc153/fibo.rb
+++ abc153/fibo.rb
@@ -1,9 +1,12 @@
-def fibo(n)
+def fibo(n, memo)
   return 0 if n == 0
   return 1 if n == 1
-  fibo(n - 1) + fibo(n - 2)
+  return memo[n] if memo[n] # メモをチェック (既に計算済みなら答えをリターンする)
+  memo[n] = fibo(n - 1, memo) + fibo(n - 2, memo)
 end
 
-(0..10).each do |n|
-  puts "#{n}項目の値: #{fibo(n)}"
+(0..50).each do |n|
+  memo = []
+  puts "#{n}項目の値: #{fibo(n, memo)}"
+  p memo
 end
